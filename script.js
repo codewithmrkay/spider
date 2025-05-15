cee040e193353a0de841cb5849f7392b021d1edf
karan <kkadam58707@gmail.com>
2025-05-15 12:03:51 +0530

    html

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
 document.addEventListener("DOMContentLoaded", (event) => {
-    gsap.registerPlugin(Draggable,ScrollTrigger,ScrollSmoother)
+    gsap.registerPlugin(Draggable, ScrollTrigger, ScrollSmoother)
     ScrollSmoother.create({
         smooth: 2,
         effects: true,
-      });
-   });
+    });
+    gsap.registerPlugin(ScrollTrigger) 
+    gsap.to('.box', {
+        x: 500,
+        scrollTrigger: {
+            trigger: ".box",
+            start: "top center",
+            end: "bottom center",
+            scrub: true,
+            markers: true
+        }
+    });
+});
